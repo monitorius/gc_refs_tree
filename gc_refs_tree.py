a602dda3e96224b685aa25a4f5ab5c62acc047db
Alexey A. Danilov <alexey.a.danilov@gmail.com>
2013-06-03 18:10:14 +0600

    print_referrers_with_keys

--- gc_refs_tree.py
+++ gc_refs_tree.py
@@ -1,7 +1,6 @@
 __all__ = ["get_referrers_tree", "print_referrers"]
 
 import gc
-import sys
 import inspect
 
 
@@ -75,6 +74,45 @@ def print_referrers(obj, max_depth, print_contents=True, shift_str="\t"):
     print_hierarchy(referrers)
 
 
+def find_reference(parent, child):
+    refs = []
+
+    if parent is None:
+        return refs
+
+    if isinstance(parent, dict):
+        for k, v in parent.items():
+            if k is child:
+                refs.append("dict key")
+            if v is child:
+                refs.append("value at <<{0}>> key".format(k))
+
+    if isinstance(parent, (list, tuple)):
+        for i, v in enumerate(parent):
+            if v is child:
+                refs.append("value at <<{0}>> key".format(i))
+
+    for name, member in inspect.getmembers(parent):
+        if member is child:
+            refs.append("member at <<{0}>> attribute".format(name))
+    return refs
+
+
+def print_referrers_with_keys(obj, max_depth, shift_str="\t"):
+    referrers = get_referrers_tree(obj, max_depth)
+    non_local = {"shift": 0}  # emulate nonlocal statment for Python2
+
+    def print_hierarchy(referrers, child):
+        current_shift = non_local["shift"] * shift_str
+        non_local["shift"] += 1
+        for ref, sub_referrers in referrers:
+            print("{0}{1} {2} {3}".format(current_shift, id(ref), type(ref), find_reference(ref, child)))
+            print_hierarchy(sub_referrers, ref)
+        non_local["shift"] -= 1
+
+    print_hierarchy(referrers, obj)
+
+
 if __name__ == "__main__":
     l = [666, ]
     d = {"d dict": l, "sub list key": ["sub list", l, l], "sub dict key": {"sub dict": l}}
@@ -82,4 +120,15 @@ if __name__ == "__main__":
     t = ("tuple", l)
     del l
 
-    print_referrers(t[1], 3, print_contents=True)
+    class A(object):
+        def __init__(self, v):
+            self.v = v
+
+    l = [444]
+    a = A(l)
+    del l
+
+    #print_referrers(l, 2, print_contents=1)
+    print_referrers_with_keys(a.v, 2)
+    #print find_reference(a.__dict__, a.v)
+
